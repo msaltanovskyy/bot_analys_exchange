c7da799f72f753557370cdfaa0c0623e0ad70627
msaltanovskyy <bust.cot@gmail.com>
2026-09-19 03:23:08 +0200

    change api

--- services/connect_to_exchange.py
+++ services/connect_to_exchange.py
@@ -4,10 +4,19 @@ import ccxt.async_support as ccxt
 class ConnectToExchange:
 
     def __init__(self) -> None:
-        self.exchange = ccxt.binance({
-            "enableRateLimit": True,
-        })
-
+      self.exchange = ccxt.binance({
+        'enableRateLimit': True,
+        'options': {
+          'defaultType': 'spot',
+        },
+        # Подключаем альтернативный домен API, не блокируемый в США
+        'urls': {
+          'api': {
+            'public': 'https://api1.binance.com/api/v3',
+            'private': 'https://api1.binance.com/api/v3',
+          }
+        }
+      })
     def get_exchange(self):
         return self.exchange
 
